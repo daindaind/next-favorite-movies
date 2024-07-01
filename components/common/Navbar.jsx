bfb98c98781ee16ae6e0ce59662b0a44930f0430
daindaind <cdin23@naver.com>
2024-07-01 17:37:03 +0900

    Style: navbar 스타일링

--- components/common/Navbar.jsx
+++ components/common/Navbar.jsx
@@ -1,19 +1,29 @@
 import { PAGE_URL } from "@/constants/router";
+import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
+import logo from '../../public/logo.png';
 
 function Navbar() {
+  const currentPath = usePathname();
+  console.log(currentPath)
   return (
-    <ul>
-      <li>
-        <Link href={PAGE_URL.BASE}>메인 화면</Link>
-      </li>
-      <li>
-        <Link href={PAGE_URL.MOVIES}>나의 영화</Link>
-      </li>
-      <li>
-        <Link href={PAGE_URL.POST}>포스팅</Link>
-      </li>
+    <ul className="flex flex-row justify-center align-middle h-15 p-5 bg-light-white">
+      <Link href={PAGE_URL.BASE}>
+        <p>LOGO</p>
+      </Link>
+      <div className="flex flex-row justify-end align-middle flex-1 gap-4">
+        <li>
+          <Link href={PAGE_URL.BASE} className={currentPath === PAGE_URL.BASE ? "font-bold text-cherry-pink no-underline" : "flex font-medium text-default-text no-underline"}>메인 화면</Link>
+        </li>
+        <li>
+          <Link href={PAGE_URL.MOVIES} className={currentPath === PAGE_URL.MOVIES ? "font-bold text-cherry-pink no-underline" : "flex font-medium text-default-text no-underline"}>나의 영화</Link>
+        </li>
+        <li>
+          <Link href={PAGE_URL.POST} className={currentPath === PAGE_URL.POST ? "font-bold text-cherry-pink no-underline" : "flex font-medium text-default-text no-underline"}>포스팅</Link>
+        </li>
+      </div>
     </ul>
   );
 }
--- constants/router.js
+++ constants/router.js
@@ -1,8 +1,8 @@
 const PAGE_URL = {
   BASE: "/",
-  MOVIES: "movies",
-  MOVIE_DETAIL: `movies/detail`,
-  POST: "post",
+  MOVIES: "/movies",
+  MOVIE_DETAIL: `/movies/detail`,
+  POST: "/post",
 };
 
 export { PAGE_URL };
