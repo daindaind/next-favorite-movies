fb440898e551afba97497ff7ad6050043cc49eeb
daindaind <cdin23@naver.com>
2024-07-02 20:03:03 +0900

    Feat: Home ThirdUI 컴포넌트 스타일링

--- components/home/ThirdUI.jsx
+++ components/home/ThirdUI.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 function ThirdUI() {
   return (
-    <div>ThirdUI</div>
+    <div className='flex flex-col justify-center items-center mb-[100px]'>
+      <h1 className='text-5xl font-bold text-default-text z-[100]'>인기 게시글</h1>
+      <h3 className='text-xl font-medium text-default-text mt-3 mb-20 z-[100]'>모두의 취향이 담긴 소개를 읽어보세요</h3>
+      
+      <div className='grid-cols-2 gap-5'>
+        {new Array(4).fill(null).map((item, index) => (
+            <div key={index}></div>
+        ))}
+      </div>
+    </div>
   )
 }
 
