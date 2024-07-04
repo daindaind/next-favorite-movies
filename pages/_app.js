1fd2ed8a4616fee2ca5b9dca903abfb3446f2c3e
daindaind <cdin23@naver.com>
2024-07-05 05:04:36 +0900

    Feat: Loading 컴포넌트, useLoading hook 구현

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
+import LoadingSpinner from "@/components/common/LoadingSpinner";
+import { useLoading } from "@/hooks/useLoading";
 import HomeLayout from "@/layouts/HomeLayout";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
+  const isLoading = useLoading();
   return (
     <HomeLayout>
+      {isLoading ? <LoadingSpinner /> : null}
       <Component {...pageProps} />
     </HomeLayout>
   );
